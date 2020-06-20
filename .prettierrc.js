c55b7c817c15f9e29c94cc462753348d85ece7d8
Miguel Faggioni <miguelfaggioni@gmail.com>
2020-06-19 22:01:21 -0400

    adding node to ignore files and prettier deps

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	tabWidth: 4,
+	semi: true,
+	useTabs: true,
+	jsxSingleQuote: false,
+	arrowParens: 'avoid',
+};
